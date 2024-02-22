69a98859d7f679e63f067bed1506c251c06ad86c
Marcelo <marceloalarconbarrenechea@gmail.com>
2024-02-22 19:26:35 -0400

    add job

---
+++ app/jobs/test23_job.rb
@@ -0,0 +1,7 @@
+class Test23Job < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ app/jobs/webhook_job.rb
@@ -0,0 +1,7 @@
+class WebhookJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
